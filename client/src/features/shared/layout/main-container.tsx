b495ac7b4fdfd5e7d5f72bb9ab71ac6fe37ac061
nhatquang1293 <nhatquang1293@gmail.com>
2022-05-30 16:31:42 +0700

    remove test query

--- client/src/features/shared/layout/main-container.tsx
+++ client/src/features/shared/layout/main-container.tsx
@@ -9,7 +9,7 @@ const MainContainer: FunctionComponent<MainContainerProps> = ({ children }) => {
     <>
       <header className='bg-white shadow'>
         <div className='mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8'>
-          <h1 className='text-3xl font-bold text-gray-900'>Dashboard</h1>
+          <h1 className='text-3xl font-bold text-gray-900'>Home</h1>
         </div>
       </header>
       <main>
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,9 +2,24 @@ import type { ReactElement } from 'react';
 import type { NextPageWithLayout } from 'next';
 
 import { Layout } from '@features/shared';
+import { useUsersQuery } from '@graphql/generrated/graphql';
 
 const Index: NextPageWithLayout = () => {
-  return <span>hihihi</span>;
+  const [usersResult, _] = useUsersQuery();
+  const { data, fetching, error } = usersResult;
+
+  if (fetching) return <h3>Loading ....</h3>;
+  if (error) return <p className='font-bold text-red-500'>{error.message}</p>;
+
+  return (
+    <>
+      {data?.users?.map(item => (
+        <span>
+          {item?.id} - {item?.username}
+        </span>
+      ))}
+    </>
+  );
 };
 
 export default Index;
